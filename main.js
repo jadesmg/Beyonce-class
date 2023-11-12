167267e4879bd0e5fd92edbe252f6cc6ae1cf386
jadesmg <138259329+jadesmg@users.noreply.github.com>
2023-11-12 01:17:16 +0000

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const correctAnswers = ['a', 'c', 'd', 'c', 'a'];
 //event listener for the submit button
 quizForm.addEventListener('submit', e => {
   e.preventDefault();
+  
  
 //function to add correct answers for a total score
   let score = 0;
@@ -18,4 +19,21 @@ quizForm.addEventListener('submit', e => {
   });
   //print out of the final score
   resultDiv.innerHTML = `Your score is ${score}/${correctAnswers.length}`;
+});
+
+
+
+//latest news - open link via button (didnt work the same as the others) - window.open("URL"), "_blank" - the blank opens it in new tab
+function relocate_home()
+{
+    window.open("https://www.hollywoodreporter.com/news/general-news/beyonce-premiere-renaissance-concert-film-london-los-angeles-1235628402/", "_blank");
+} 
+
+
+
+
+
+
+lightbox.option ({
+    alwaysShowNavOnTouchDevices: true
 });
